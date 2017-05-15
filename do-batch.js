72e556a47c15662d2eef81cbb5b8aabdefe0c4f4
nathanallen <thenathanator@gmail.com>
2017-05-15 15:35:08 -0700

    fix array example

--- do-batch.js
+++ do-batch.js
@@ -1,6 +1,7 @@
 var batch_input = [
-  "pLe*A/tED SKirt",
-  "f*AncY T/Ie"
+  [ "pLe*A/tED SKirt", "f*AncY T/Ie" ],
+  ["cumber*bund"],
+  ["rEd tuX/edo", "red* soCks"]
 ];
 
 /* TODO: copy `clean` function here */
